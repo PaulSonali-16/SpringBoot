51a813c63ade97367779ee473a3783addb7205f6
Paulsonali <87829318+Paulsonali@users.noreply.github.com>
2023-04-09 21:24:45 +0100

    sayali

--- src/main/java/com/dept/GitDemoApplication.java
+++ src/main/java/com/dept/GitDemoApplication.java
@@ -5,10 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GitDemoApplication {
-
+	private Map<String,Object> cachesss=new HashMap<String,Object>();
 	public static void main(String[] args) {
 		SpringApplication.run(GitDemoApplication.class, args);
 		int a=10;
 	}
-
+	public void loadDataToCache(){
+		//logic
+	}
 }
+
+
